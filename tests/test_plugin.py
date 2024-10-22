f0b106fc5da7346625ded6e6802a0f93d7507ed7
Gerben Oostra <gerben.oostra@gmail.com>
2024-10-22 08:44:10 +0200

    test: verify modify_locked_package_to_name

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import pytest
 from poetry.core.packages.package import Package
 from poetry.factory import Factory
 
-from poetry_plugin_mono_repo_deps.plugin import ALLOWED_CONSTRAINTS, Config, create_named_dependency
+from poetry_plugin_mono_repo_deps.plugin import (
+    ALLOWED_CONSTRAINTS,
+    Config,
+    create_named_dependency,
+    modify_locked_package_to_named,
+)
+from tests.helpers import prepare_test_poetry
 
 
 def test_config_empty() -> None:
@@ -84,3 +92,81 @@ def test_named_dependency_for_package_with_files(constraint: str) -> None:
     package_a2 = Package("A", "2.0.0")
     package_a2.files = [{"file": "baz", "hash": "345"}]
     create_named_dependency(constraint, package_a2.to_dependency(), package_a2)
+
+
+def test_nested_dependency() -> None:
+    package_a = Package("A", "1.0.0")
+    package_a.files = [{"file": "baz", "hash": "345"}]
+    package_b = Package("B", "1.0.0")
+    package_b.add_dependency(package_a.to_dependency())
+    dep = create_named_dependency("~=", package_b.to_dependency(), package_b)
+    assert dep is not None
+
+
+def test_lock_to_name_files(fixture_simple_a: Path) -> None:
+    poetry = prepare_test_poetry(fixture_simple_a / "lib-a")
+    package_a = Package("A", "1.0.0")
+    package_a.add_dependency(Factory.create_dependency("B", "^1.0"))
+    package_a.files = [{"file": "foo", "hash": "456"}, {"file": "bar", "hash": "123"}]
+    locked_packages = poetry._locker._lock_packages([package_a])
+    modify_locked_package_to_named(locked_packages[0])
+    assert locked_packages[0] == {
+        "name": "A",
+        "version": "1.0.0",
+        "files": [{"file": "bar", "hash": "123"}, {"file": "foo", "hash": "456"}],
+        "dependencies": {"B": "^1.0"},
+        "description": "",
+        "optional": False,
+        "python-versions": "*",
+    }
+
+
+def test_lock_to_name_for_path_dependency(fixture_simple_a: Path) -> None:
+    poetry = prepare_test_poetry(fixture_simple_a / "lib-a")
+    package_a = Package("A", "1.0.0", source_type="directory", source_url="../lib-a")
+    locked_packages = poetry._locker._lock_packages([package_a])
+    modify_locked_package_to_named(locked_packages[0])
+    assert locked_packages[0] == {
+        "name": "A",
+        "version": "1.0.0",
+        "description": "",
+        "files": [],
+        "optional": False,
+        "python-versions": "*",
+    }
+
+
+def test_lock_to_name_for_transitive_path_dependency(fixture_simple_a: Path) -> None:
+    poetry = prepare_test_poetry(fixture_simple_a / "lib-a")
+    package_a = Package("A", "1.0.0", source_type="directory", source_url="../lib-a")
+    package_b = Package("B", "1.0.0")
+    package_b.add_dependency(package_a.to_dependency())
+    locked_packages = poetry._locker._lock_packages([package_b])
+    modify_locked_package_to_named(locked_packages[0])
+    assert locked_packages[0] == {
+        "name": "B",
+        "version": "1.0.0",
+        "dependencies": {"a": {"version": "*"}},
+        "description": "",
+        "files": [],
+        "optional": False,
+        "python-versions": "*",
+    }
+
+
+def test_lock_to_name_for_transitive_path_dependency_with_extras(fixture_simple_a: Path) -> None:
+    poetry = prepare_test_poetry(fixture_simple_a / "lib-a")
+    package_a = Package("A", "1.0.0", source_type="directory", source_url="../lib-a", features=["foo"])
+    package_b = Package("B", "1.0.0")
+    package_b.add_dependency(package_a.to_dependency())
+    locked_packages = poetry._locker._lock_packages([package_b])
+    modify_locked_package_to_named(locked_packages[0])
+    assert locked_packages[0] == {
+        "name": "B",
+        "version": "1.0.0",
+        "dependencies": {"a": {"extras": ["foo"], "version": "*"}},
+        "description": "",
+        "files": [],
+        "optional": False,
+        "python-versions": "*",
+    }
